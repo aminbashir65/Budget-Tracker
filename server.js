0239e759582ecc6391419275a6f3742f98fc3f1e
amin bashir <aminbashir65@gmail.com>
2021-05-26 16:01:50 -0500

    Changes made

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT =process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+const dbURI = process.env.mongodbURI || "mongodb://localhost/budget"
+mongoose.connect(dbURI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
